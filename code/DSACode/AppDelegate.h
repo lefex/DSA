46a77ed2bb66b8ab91807a9a43d6260b6dfa2e04
Wang,Suyan <wangsuyan@baidu.com>
2018-04-10 08:57:12 +0800

    开启项目

---
+++ code/DSACode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DSACode
+//
+//  Created by Wang,Suyan on 2018/4/10.
+//  Copyright © 2018年 Wang,Suyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
